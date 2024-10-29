92b8acf78b76ce9dbb78307c4625e2389a75d15c
Guillaume Paoli <2142678@cegepoutaouais.qc.ca>
2024-10-29 11:15:11 -0400

    resolution de plusieur bug

---
+++ commande/d100.py
@@ -0,0 +1,2 @@
+async def run(interraction):
+    await interraction.response.send_message("Oui bas j'ai pas encore code le d100 >~<")
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 from discord import app_commands
+from discord.ext import commands
+from commande.d100 import run as d100
 
 #propriete
 contenu = ''
@@ -7,6 +9,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client=bot)
+bot.tree = tree
 
 #recuper le token
 with open('token.txt', 'r') as fichier:
@@ -15,20 +18,20 @@ with open('token.txt', 'r') as fichier:
 #log
 @bot.event
 async def on_ready():
-    await tree.sync(guild=discord.Object(id=587086433970552840))
+    GUILD_IDS = [587086433970552840,1183006208869797898]
+    for guild_id in GUILD_IDS:
+        guild = discord.Object(id=guild_id)
+        await tree.sync(guild=guild)
     print("Le bot est prêt !")
 
-
-#message 
-@bot.event
-async def on_message(message: discord.Message):
-    print(message.guild.id)
-
 #commande
-@tree.command(name = "allo", description="Un simple Hello World!",guild=discord.Object(id=587086433970552840))
+@bot.tree.command(name = "allo", description="Un simple Hello World!",)
 async def allo(interaction):
     await interaction.response.send_message("Hello World!")
 
+@bot.tree.command(name="d100", description="lance un 1d100")
+async def command_d100(interaction):
+    await d100(interaction)
 
 #run le bot
 bot.run(contenu)
