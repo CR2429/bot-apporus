81c9b40c4128cf38a9c06eb48ea18e8a509ce9db
CR2429 <WDgaster.gtp@gmail.com>
2024-10-29 11:50:52 -0400

    update d100

--- commande/d100.py
+++ commande/d100.py
@@ -1,2 +1,79 @@
+import random
+
 async def run(interraction):
-    await interraction.response.send_message("Oui bas j'ai pas encore code le d100 >~<")
+    #chjoix du chiffre random
+    result = random.randint(1,100)
+
+    #message different celon le chiffre
+    if result == 1:
+        message = (
+            '```\n'
+            '╔══════════════════════╗\n'
+            '║    Jet d\'un d100     ║\n'  
+            '╟──────────────────────╢\n'
+            '║ CRITIQUE ABSOLUE (1) ║\n'
+            '╚══════════════════════╝\n'
+            '```'
+        )
+    elif 2 <= result <= 5:
+        message = (
+            '```\n'
+            '╔══════════════════════╗\n'
+            '║    Jet d\'un d100     ║\n'  
+            '╟──────────────────────╢\n'
+            f'║ SUCCÈS CRITIQUE ({result})  ║\n'
+            '╚══════════════════════╝\n'
+            '```'
+        )
+    elif 96 <= result <= 99:
+        message = (
+            '```\n'
+            '╔══════════════════════╗\n'
+            '║    Jet d\'un d100     ║\n'  
+            '╟──────────────────────╢\n'
+            f'║ ÉCHEC CRITIQUE ({result})  ║\n'
+            '╚══════════════════════╝\n'
+            '```'
+        )
+    elif result == 100:
+        message = (
+            '```\n'
+            '╔══════════════════════╗\n'
+            '║    Jet d\'un d100     ║\n'  
+            '╟──────────────────────╢\n'
+            '║ ÉCHEC ABSOLU (100)   ║\n'
+            '╚══════════════════════╝\n'
+            '```'
+        )
+    elif result == 69:
+        message = (
+            '```\n'
+            '╔══════════════════════╗\n'
+            '║    Jet d\'un d100     ║\n'  
+            '╟──────────────────────╢\n'
+            '║    Résultat : 69     ║\n'
+            '╚══════════════════════╝\n'
+            '```'
+        )
+    elif 6 <= result <= 9:
+        message = (
+            '```\n'
+            '╔══════════════════════╗\n'
+            '║    Jet d\'un d100     ║\n'  
+            '╟──────────────────────╢\n'
+            f'║     Résultat : {result}     ║\n'
+            '╚══════════════════════╝\n'
+            '```'
+        )
+    else:
+        message = (
+            '```\n'
+            '╔══════════════════════╗\n'
+            '║    Jet d\'un d100     ║\n'  
+            '╟──────────────────────╢\n'
+            f'║    Résultat : {result}     ║\n'
+            '╚══════════════════════╝\n'
+            '```'
+        )
+    
+    await interraction.response.send_message(message)
